e52cc3996fdde355c97666674b2d4b12ba6b4f9d
yungan9 <geyun105@naver.com>
2025-06-07 17:43:49 +0900

    feat: support functional components in createElement - createElement 함수에서 태그가 함수일 경우 직접 호출 - 함수형 컴포넌트를 사용해 가상 DOM 반환

--- app.js
+++ app.js
@@ -2,17 +2,22 @@
 // ---- Library ---
 const React = {
     createElement: (tag, props, ...children) => {
+        if (typeof tag === 'function') {
+            return tag(props, ...children);
+        }
         const el = {
             tag,
             props,
             children,
         };
-        console.log(el);
         return el;
     },
 };
 // ---- Application ---
-const App = (React.createElement("div", { draggable: true },
-    React.createElement("h2", null, "Hello React!"),
-    React.createElement("p", null, "I am a pargraph"),
-    React.createElement("input", { type: "text" })));
+const App = () => {
+    return (React.createElement("div", { draggable: true },
+        React.createElement("h2", null, "Hello React!"),
+        React.createElement("p", null, "I am a pargraph"),
+        React.createElement("input", { type: "text" })));
+};
+console.log(React.createElement(App, null));
--- app.tsx
+++ app.tsx
@@ -2,20 +2,25 @@
 // ---- Library ---
 const React = {
     createElement: (tag, props, ...children) => {
+        if (typeof tag === 'function') {
+            return tag(props, ...children);
+        }
         const el = {
             tag,
             props,
             children,
         };
-        console.log(el);
         return el;
     },
 };
 // ---- Application ---
-const App = (
-    <div draggable>
-        <h2>Hello React!</h2>
-        <p>I am a pargraph</p>
-        <input type="text" />
-    </div>
-);
+const App = () => {
+    return (
+        <div draggable>
+            <h2>Hello React!</h2>
+            <p>I am a pargraph</p>
+            <input type="text" />
+        </div>
+    );
+};
+console.log(<App />);
